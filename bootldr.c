1555660fd33c0dd7ab88a11bed16f186428961f1
Steven LeBlanc <skleblan@gmail.com>
2019-10-16 12:25:55 -0500

    Architect Bootloader code
    Started architecting and drafting out the code for the bootloader.
    
    Created a definition for the data packets (however, i think i
    might re-consider my rules for 8-byte data)
    
    Nothing has been compiled.  Need to double check my usage of
    ENUMS, UNIONS, STRUCTS, and POINTERS.

---
+++ bootldr.c
@@ -0,0 +1,67 @@
+#include "uart.h"
+
+enum bootldr_pkt_type
+{
+  addr_start,
+  data,
+  branch_cmd
+};
+
+struct bootldr_pkt_struct
+{
+  char begin_sync;
+  char datalen;
+  char invdatalen;
+  enum bootldr_pkt_type type;
+  unsigned char sequence;
+  char passfail;
+  unsigned char data[8];
+  char end_sync;
+  char cksum;
+};
+
+#if 0
+union bootldr_pkt
+{
+  unsigned char bytes[12];
+  struct bootldr_pkt_struct struct;
+};
+#endif
+
+typedef struct bootldr_pkt_struct bootldr_pkt_t;
+
+void load_new_program()
+{
+  char buffer[0xF]; //bigger buffer?
+  int rcvd_len = 0;
+  bootldr_pkt_t* packet;
+  char* cur_start_addr;
+
+  while(1)
+  {
+    read(buffer, 0xF);
+    packet = buffer;
+    
+    //check packet sync. begin and end
+    
+    //check checksum
+    
+    //check inverted datalength
+    
+    switch(packet.type)
+    {
+      case addr_start:
+	cur_start_addr = 0;
+	for(i = 0; i < 4; i++)
+	{
+	  cur_start_addr |= packet.data[i]<<(8*(i-1));
+	}
+	break;
+      case data:
+	break;
+case branch_cmd:
+	break;
+    }
+
+
+
