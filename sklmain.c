db610328e754eff00ffd4bf2c438c0025a9724e7
Steven LeBlanc <skleblan@gmail.com>
2019-10-17 17:21:10 -0500

    Toggle LED Menu command
    Created a new menu command to toggle one of the LED's built in
    to the raspberry pi board.

--- sklmain.c
+++ sklmain.c
@@ -1,6 +1,10 @@
 #include "uart.h"
 #include "util.h"
+#include "vectors.h"
 
+#define GPFSEL1 0x20200004
+#define GPSET0  0x2020001C
+#define GPCLR0  0x20200028
 
 #define BUFF_SIZE 100
 unsigned char buffer[BUFF_SIZE];
@@ -12,8 +16,10 @@ unsigned char msg[] = "Thank You";
 #define MENUCMD_ECHO       0x03
 #define MENUCMD_BOOTLDR    0x04
 #define MENUCMD_MARCO      0x05
+#define MENUCMD_LED        0x06
 
-unsigned int menucmdlistsz = 6;
+unsigned int menucmdlistsz = 7;
+#define MAX_CMD_STRING_LENGTH 0xF
 const char* menucmdlist[] = 
 {
   "\0", //empty
@@ -21,8 +27,38 @@ const char* menucmdlist[] =
   "\0", //unknown
   "echo\0",
   "bootldr\0",
-  "marco\0"
+  "marco\0",
+  "led\0"
 };
+
+char led_status = 0;
+
+void init_led(void)
+{
+  unsigned int ra;
+
+  ra=GET32(GPFSEL1);
+  ra&=~(7<<18);
+  ra|=1<<18;
+  PUT32(GPFSEL1,ra);
+
+  writeln("led init complete", 17);
+}
+
+void toggle_led(void)
+{
+  led_status = ~led_status;
+  if(led_status)
+  {
+    PUT32(GPSET0,1<<16);
+    writeln("led off", 7);
+  }
+  else
+  {
+    PUT32(GPCLR0,1<<16);
+    writeln("led on", 6);
+  }
+}
 
 unsigned int rdmnucmd(char* buffer, unsigned int bufsize)
 {
@@ -32,7 +68,7 @@ unsigned int rdmnucmd(char* buffer, unsigned int bufsize)
 
   for(i = 3; i < menucmdlistsz; i++)
   {
-    cmdlen = strlen(menucmdlist[i], 0xF);
+    cmdlen = strlen(menucmdlist[i], MAX_CMD_STRING_LENGTH);
     
     is_equal = strncmp(menucmdlist[i], buffer, cmdlen);
     if(is_equal == 0)
@@ -54,6 +90,9 @@ void menumain(char* buffer, unsigned int bufsize)
     case MENUCMD_MARCO:
       writeln("polo", 4);
       break;
+    case MENUCMD_LED:
+      toggle_led();
+      break;
     case MENUCMD_ECHO:
     case MENUCMD_EMPTY:
     case MENUCMD_BOOTLDR:
@@ -68,6 +107,7 @@ void sklmain(void)
 {
   unsigned int chars_read = 0;
   init_uart();
+  init_led();
 
   while(1)
   {
